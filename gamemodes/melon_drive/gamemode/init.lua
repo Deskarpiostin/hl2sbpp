005a96d211bc25a14af9ff4b06d24d69198ac670
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-01-10 22:26:09 +0200

    melondrive: remove unnecessary func stubs from lua

--- gamemodes/melon_drive/gamemode/init.lua
+++ gamemodes/melon_drive/gamemode/init.lua
@@ -17,12 +17,6 @@ local tSpawnPointClassnames = {
   "info_player_start",
 }
 
-function GM:AddLevelDesignerPlacedObject(pEntity)
-  return false
-end
-
-function GM:AllowDamage(pVictim, info) end
-
 function GM:CanEnterVehicle(pPlayer, pVehicle, nRole) end
 
 function GM:CanHavePlayerItem(pPlayer, pItem)
@@ -33,44 +27,12 @@ function GM:CanHavePlayerItem(pPlayer, pItem)
   end
 end
 
-function GM:CanPlayerHearPlayer(pListener, pTalker, bProximity) end
-
-function GM:CheatImpulseCommands(pPlayer, iImpulse) end
-
-function GM:CheckChatForReadySignal(pPlayer, chatmsg) end
-
-function GM:CleanUpMap() end
-
-function GM:ClientConnected(pEntity, pszName, pszAddress, reject, maxrejectlen) end
-
-function GM:ClientDisconnected(pClient) end
-
 function GM:FlItemRespawnTime(pItem)
   return cvar.FindVar("sv_hl2mp_item_respawn_time"):GetFloat()
 end
 
-function GM:FlPlayerFallDamage(pPlayer) end
-
-function GM:FlPlayerSpawnTime(pPlayer) end
-
-function GM:FPlayerCanRespawn(pPlayer) end
-
-function GM:FPlayerCanTakeDamage(pPlayer, pAttacker) end
-
-function GM:FShouldSwitchWeapon(pPlayer, pWeapon) end
-
-function GM:GetPlayerHandModel(player, handStr, soundType) end
-
 function GM:GiveDefaultItems(pPlayer)
   --pPlayer:EquipSuit();
-end
-
-function GM:Host_Say(pPlayer, p, teamonly) end
-
-function GM:InitHUD(pPlayer) end
-
-function GM:ItemShouldRespawn(pItem)
-  return false
 end
 
 function GM:LevelInit(strMapName, strMapEntities, strOldLevel, strLandmarkName, loadGame, background)
@@ -87,16 +49,6 @@ function GM:LevelInit(strMapName, strMapEntities, strOldLevel, strLandmarkName, 
   end
 end
 
-function GM:NetworkIDValidated(strUserName, strNetworkID) end
-
-function GM:PlayerCanHearChat(pListener, pSpeaker) end
-
-function GM:PlayerCanPickupObject(pObject, massLimit, sizeLimit) end
-
-function GM:PlayerDeathSound(info) end
-
-function GM:PlayerDeathThink(pPlayer) end
-
 function GM:PlayerEntSelectSpawnPoint(pHL2MPPlayer)
   local tSpawnPoints = {}
   local pSpot = NULL
@@ -109,10 +61,6 @@ function GM:PlayerEntSelectSpawnPoint(pHL2MPPlayer)
   end
   return tSpawnPoints[math.random(1, #tSpawnPoints)]
 end
-
-function GM:PlayerGotItem(pPlayer, pItem) end
-
-function GM:PlayerInitialSpawn(pPlayer) end
 
 function GM:PlayerPickupObject(pHL2MPPlayer, pObject, bLimitMassAndSize)
   return false
@@ -172,20 +120,6 @@ function GM:PlayerThink(pPlayer)
   pPlayer:SetLocalOrigin(pPlayer.CurrentKartPos - Vector(0, 0, 70))
   pPlayer:SetFOV(pPlayer, 100, 0, 0)
 end
-
-function GM:RemoveLevelDesignerPlacedObject(pEntity) end
-
-function GM:RestartGame() end
-
-function GM:ServerActivate(edictCount, clientMax) end
-
-function GM:ShouldHideServer() end
-
-function GM:VecItemRespawnSpot(pItem) end
-
-function GM:VecItemRespawnAngles(pItem) end
-
-function GM:WeaponShouldRespawn(pWeapon) end
 
 function GM:Weapon_Equip(pPlayer, pWeapon)
   pPlayer:StripWeapons()
